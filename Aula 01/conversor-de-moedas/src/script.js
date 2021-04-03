1572f0d5680a0ee9f94664f99ddfabfec8bb1bfc
Cleyciane Farias <fariascleycianedelima@gmail.com>
2021-04-03 14:42:12 -0500

    Aula 01

---
+++ Aula 01/conversor-de-moedas/src/script.js
@@ -0,0 +1,16 @@
+var valorEmDolarTexto = prompt("Qual o valor em dólar você quer converter?")
+
+var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
+
+alert(valorEmDolarNumero)
+
+var valorEmReal = valorEmDolarNumero * 5.50
+var valorEmRealDecimal = valorEmReal.toFixed(2)
+
+alert(valorEmRealDecimal)
+
+//Revisão
+//variáveis var int - float - string
+//alert - parseInt - parseFloat - prompt
+//operações + somar * multiplicar
+//comentário
